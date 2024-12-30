d7758d8e83d50d3ea320e328358c7eb3587a0e77
loadigital <95947050+loadigital@users.noreply.github.com>
2024-12-30 14:04:48 -0500

    Update header.js

--- src/include/header.js
+++ src/include/header.js
@@ -5,14 +5,14 @@ divHeader.innerHTML = `
 <header>
     
     <div class="menu_bar">
-        <div class="bt-menu"><img src="https://www.biloa.store/img/LOGO11.png" alt=""><span class="bi-list" id="menu_bar"></span></div>
+        <div class="bt-menu"><img src="https://loadigital.github.io/video360p/img/LOGO12.png" alt=""><span class="bi-list" id="menu_bar"></span></div>
      </div>
         
         <nav>
             <ul class="ul-main">
                 
                 <div class="contenedor_list flexrow-c col-12">
-                    <a class="flexrow-c a-lo" href="#" data="next"><img class="img-logo" src="https://www.biloa.store/img/LOGO11.png" alt=""></a>
+                    <a class="flexrow-c a-lo" href="#" data="next"><img class="img-logo" src="https://loadigital.github.io/video360p/img/LOGO12.png" alt=""></a>
                     
                     <li class="next"><a href="https://loadigital.github.io/video360p/" data="next"><span class="bi-house"></span>Inicio</a></li>
             
@@ -21,22 +21,22 @@ divHeader.innerHTML = `
                     
                     <li class="btn--"  id="btn--"><a href="#" data="next"><span class="bi-collection-play-fill"></span>Genero</a>
                         <ul class="hidden" id="hidden">
-                            <li class="next"><a href="/src/genres/genero?g=28" data="next"><span class="bi-folder2-open"></span>Acción</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=16" data="next"><span class="bi-folder2-open"></span>Animación</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=12" data="next"><span class="bi-folder2-open"></span>Aventura</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=10752" data="next"><span class="bi-folder2-open"></span>Bélico</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=878" data="next"><span class="bi-folder2-open"></span>Ciencia ficción</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=35" data="next"><span class="bi-folder2-open"></span>Comedia</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=80" data="next"><span class="bi-folder2-open"></span>Crimen</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=99" data="next"><span class="bi-folder2-open"></span>Documental</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=18" data="next"><span class="bi-folder2-open"></span>Drama</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=10751" data="next"><span class="bi-folder2-open"></span>Familia</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=14" data="next"><span class="bi-folder2-open"></span>Fantasia</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=36" data="next"><span class="bi-folder2-open"></span>Historia</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=9648" data="next"><span class="bi-folder2-open"></span>Misterio</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=10749" data="next"><span class="bi-folder2-open"></span>Romance</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=53" data="next"><span class="bi-folder2-open"></span>Suspenso</a></li>
-                            <li class="next"><a href="/src/genres/genero?g=27" data="next"><span class="bi-folder2-open"></span>Terror</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=16" data="next"><span class="bi-folder2-open"></span>Animación</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=12" data="next"><span class="bi-folder2-open"></span>Aventura</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=10752" data="next"><span class="bi-folder2-open"></span>Bélico</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=878" data="next"><span class="bi-folder2-open"></span>Ciencia ficción</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=35" data="next"><span class="bi-folder2-open"></span>Comedia</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=80" data="next"><span class="bi-folder2-open"></span>Crimen</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=99" data="next"><span class="bi-folder2-open"></span>Documental</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=18" data="next"><span class="bi-folder2-open"></span>Drama</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=10751" data="next"><span class="bi-folder2-open"></span>Familia</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=14" data="next"><span class="bi-folder2-open"></span>Fantasia</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=36" data="next"><span class="bi-folder2-open"></span>Historia</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=9648" data="next"><span class="bi-folder2-open"></span>Misterio</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=10749" data="next"><span class="bi-folder2-open"></span>Romance</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=53" data="next"><span class="bi-folder2-open"></span>Suspenso</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=27" data="next"><span class="bi-folder2-open"></span>Terror</a></li>
+                            <li class="next"><a href="/video360p/src/genres/genero?g=28" data="next"><span class="bi-folder2-open"></span>Acción</a></li>
     
                             
                         </ul>
@@ -55,8 +55,8 @@ divHeader.innerHTML = `
                     
                     <li class="btn--"><a id="btn--" href="#" data="next"><span class="bi-search"></span>Buscador...</a>
                         <ul class="hidden" id="hidden">
-                            <li class="next"><a href="/search&id=uno" data="next"><span class="bi-folder2-open"></span>Películas</a></li>
-                            <li class="next"><a href="/search&id=dos" data="next"><span class="bi-folder2-open"></span>Series</a></li>
+                            <li class="next"><a href="/video360p/search&id=uno" data="next"><span class="bi-folder2-open"></span>Películas</a></li>
+                            <li class="next"><a href="/video360p/search&id=dos" data="next"><span class="bi-folder2-open"></span>Series</a></li>
                         </ul>
                     
                     </li>
