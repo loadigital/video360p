45a5530f7acfbb651a27ed2220918cea69d9bebc
loadigital <95947050+loadigital@users.noreply.github.com>
2024-12-30 11:59:32 -0500

    Update api.js

--- js/api.js
+++ js/api.js
@@ -3,9 +3,9 @@ const  BASE_URL  =  'https://api.themoviedb.org/3' ;
 const  IMG_URL  =  'https://image.tmdb.org/t/p/w500' ;
 
 
-const linkDirect = "https://www.biloa.store/";
-const linkMovies = "https://www.biloa.store/page/embeduno.php?id="; 
-const linkSeries = "http://localhost/B!LOA%20APP%200.5/page/embeddos.php?id="; 
+const linkDirect = "https://loadigital.github.io/video360p/";
+const linkMovies = "https://loadigital.github.io/video360p/src/page/peliculas.html?id="; 
+const linkSeries = "https://loadigital.github.io/video360p/src/page/series.html?id="; 
 
-const page_peliculas = "https://www.biloa.store/peliculas";
-const page_series = "https://www.biloa.store/series";
+const page_peliculas = "https://loadigital.github.io/video360p/peliculas.html";
+const page_series = "https://loadigital.github.io/video360p/series.html";
