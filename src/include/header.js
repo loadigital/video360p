f492dddbcbacc4406b1c6f6abe5d2cb71c73acd5
loadigital <95947050+loadigital@users.noreply.github.com>
2024-12-30 21:59:46 -0500

    Update header.js

--- src/include/header.js
+++ src/include/header.js
@@ -63,7 +63,7 @@ divHeader.innerHTML = `
 
                     <li class="next class-mo"><a target="_blank" href="https://loadigital.github.io/video360p/app/descargarApp.html" data="next"><span class="bi-android2"></span>Descargar App</a></li>
                     <li class="next class-mo"><a target="_blank" href="https://t.me/vid360p" data="next"><span class="bi-telegram"></span>Grupo Telegram</a></li>
-                    <li class="next class-mo"><a  href="#" data="next"><span class="bi-gear-fill"></span>Versión 0.1</a></li>
+                    <li class="next class-mo"><a  href="#" data="next"><span class="bi-gear-fill"></span>Versión 0.3</a></li>
                 </div>
                 
             </ul>
